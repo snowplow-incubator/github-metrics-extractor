85e67af34976bb005e6af5191d5c4fce5e7ff1ae
Ryan Hill <r.l.hill128@gmail.com>
2023-03-21 11:10:45 +0000

    new method

--- extract_metrics.py
+++ extract_metrics.py
@@ -2,8 +2,11 @@ import requests
 import sys
 import pandas as pd
 from datetime import date
-import pickle
-from snowplow_tracker import Snowplow, EmitterConfiguration, Tracker, Emitter, TrackerConfiguration, SelfDescribingJson
+import os
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
 
 def github_get_traffic(username, repository_name, trafic_type = 'clones', headers={}):
     headers = headers
@@ -14,7 +17,6 @@ def github_get_traffic(username, repository_name, trafic_type = 'clones', header
     data = r.json()
 
     return data
-
 
 def main():
     repos = [
@@ -29,73 +31,81 @@ def main():
 
     #Set your PAT key so you get the 5000 calls per hour for the github api
     # call with `token <YOUR_TOKEN> as cmd argument if using a local PAT key`
-    headers = {'Authorization': f"token {sys.argv[1]}"}
+    headers = {'Authorization': f"{sys.argv[1]}"}
     username = 'snowplow'
     df = pd.DataFrame()
     for repo in repos:
         # Clones
         df_temp = pd.DataFrame.from_dict(github_get_traffic(username, repo, 'clones', headers)['clones'])
         df_temp['metric'] = 'clones'
+        df_temp['repo'] = repo
         df = pd.concat([df, df_temp])
         # Popular paths
         df_temp = pd.DataFrame.from_dict(github_get_traffic(username, repo, 'popular/paths', headers)).drop(columns=['title']).rename(columns={'path':'value'})
         df_temp['metric'] = 'popular/paths'
         df_temp['timestamp'] = date.today().strftime("%Y-%m-%dT%H:%M:%SZ")
         df_temp['rank'] = range(1, len(df_temp.index) + 1)
+        df_temp['repo'] = repo
         df = pd.concat([df, df_temp])
         # Popular referrers
         df_temp = pd.DataFrame.from_dict(github_get_traffic(username, repo, 'popular/referrers', headers)).rename(columns={'referrer':'value'})
         df_temp['metric'] = 'popular/referrers'
         df_temp['timestamp'] = date.today().strftime("%Y-%m-%dT%H:%M:%SZ")
         df_temp['rank'] = range(1, len(df_temp.index) + 1)
+        df_temp['repo'] = repo
         df = pd.concat([df, df_temp])
         # Views
         df_temp = pd.DataFrame.from_dict(github_get_traffic(username, repo, 'views', headers)['views'])
         df_temp['metric'] = 'views'
+        df_temp['repo'] = repo
         df = pd.concat([df, df_temp])
-
-        df['repo'] = repo
-
-    # Get previous max date
-    try: # first run protection
-        m_tstamp = pickle.load(open('./data/last_max.pkl', 'rb'))
-    except:
-        m_tstamp = pd.to_datetime('2020-01-01T00:00:00Z')
 
     # Are there better ways to do this? Yes, of course there are.
     # Do I have more than a passing familiarity with pandas to know that way? No, I do not.
     df = df.convert_dtypes()
     df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df = df[df['timestamp'] > m_tstamp]
-    df = df.replace({pd.NA: None})
 
-    # For now just store it
     if len(df. index) > 0:
-        # df.to_csv(f'data/exported_metrics_{date.today()}.csv', index = False)
-
-        e = Emitter("localhost:9090", protocol='http')
-
-        t = Tracker(e,
-                        app_id="gh_metrics",)
-        for _, row in df.iterrows():
-            print(f"sending event: {row['metric']}")
-            t.track_self_describing_event(SelfDescribingJson(
-            "iglu:com.snowplowanalytics/github_metrics/jsonschema/1-0-0",
-            {
-                "rank": row['rank'],
-                "repo": row['repo'],
-                "value": row['value'],
-                "metric_tstamp": row['timestamp'].strftime("%Y-%m-%dT%H:%M:%S.0Z"),
-                "uniques": row['uniques'],
-                "metric": row['metric'],
-                "count": row['count']
+        con = snowflake.connector.connect(
+            user = os.getenv('SNOWFLAKE_USER'),
+            password = os.getenv('SNOWFLAKE_PASSWORD'),
+            account = os.getenv('SNOWFLAKE_ACCOUNT'),
+            warehouse = os.getenv('SNOWFLAKE_WAREHOUSE'),
+            database = os.getenv('SNOWFLAKE_DATABASE'),
+            schema = os.getenv('SNOWFLAKE_SCHEMA'),
+            role = os.getenv('SNOWFLAKE_USER_ROLE'),
+            session_parameters = {
+                'QUERY_TAG': 'githubMetrics'
             }
-            ))
+        )
 
-        t.flush()
+    # Write the data from the DataFrame to the table staging table
+        success, _, nrows, _ = write_pandas(con, df, 'metrics_stg', auto_create_table=True, quote_identifiers=False)
 
-        # Write value so it's saved
-        pickle.dump(max(df['timestamp']), open('./data/last_max.pkl', 'wb'))
+        if success:
+            print(f"Written {nrows} to `metrics_stg`")
+        else:
+            raise ValueError("Something went wrong")
+
+        # Why am I connecting again? Great question, ask Snowflake why their package only does 2 things.
+        engine = create_engine(URL(
+            account = os.getenv('SNOWFLAKE_ACCOUNT'),
+            user = os.getenv('SNOWFLAKE_USER'),
+            password = os.getenv('SNOWFLAKE_PASSWORD'),
+            database = os.getenv('SNOWFLAKE_DATABASE'),
+            schema =  os.getenv('SNOWFLAKE_SCHEMA'),
+            warehouse = os.getenv('SNOWFLAKE_WAREHOUSE'),
+            role = os.getenv('SNOWFLAKE_USER_ROLE'),
+        ))
+        connection = engine.connect()
+
+        try:
+            # there is a mergeInto function but it's more work than just writing the sql manually
+            merge = "merge into metrics t using metrics_stg s on t.metric = s.metric and t.timestamp = s.timestamp and t.repo = s.repo when not matched then insert (timestamp, count, uniques, metric, value, rank, repo) values (s.timestamp, s.count, s.uniques, s.metric, s.value, s.rank, s.repo)"
+            connection.execute(merge)
+        finally:
+            connection.close()
+            engine.dispose()
 
 
 if __name__ == '__main__':
